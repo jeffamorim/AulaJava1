85ae9b9ac1c5d302b5f28eb34f5cb8bf0a2565e9
Jeferson Amorim Leite <jamoriml@everis.com>
2020-10-21 10:48:18 -0300

    Exemplo Java

---
+++ AulaJava1/src/Aula1/Aula1.java
@@ -0,0 +1,15 @@
+package Aula1;
+
+
+
+public class Aula1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Wolrd");
+		 
+		
+		
+	}
+
+}
